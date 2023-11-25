7b6c04c3025a1be02884aa104c5b981b6fc8c83d
rkl71 <renkelin00@gmail.com>
2023-11-25 23:37:37 +0900

    命令行开发

---
+++ hello-generator-basic/src/main/java/com/hanyang/cli/pattern/Command.java
@@ -0,0 +1,5 @@
+package com.hanyang.cli.pattern;
+
+public interface Command {
+    void execute();
+}
--- hello-generator-basic/src/main/java/com/hanyang/generator/MainGenerator.java
+++ hello-generator-basic/src/main/java/com/hanyang/generator/MainGenerator.java
@@ -22,14 +22,14 @@ public class MainGenerator {
 
         String projectPath = System.getProperty("user.dir");
         // 整个项目的根路径
-        File parentFile = new File(projectPath);
+        File parentFile = new File(projectPath).getParentFile();
         // 输入路径
         String inputPath = new File(parentFile, "hello-generator-demo-projects/acm-template").getAbsolutePath();
         String outputPath = projectPath;
         // 生成静态文件
         StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
         // 生成动态文件
-        String inputDynamicFilePath = projectPath + File.separator + "hello-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String inputDynamicFilePath = projectPath + File.separator + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
         String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/hanyang/acm/MainTemplate.java";
         DynamicGenerator.doGenerator(inputDynamicFilePath, outputDynamicFilePath, model);
     }
